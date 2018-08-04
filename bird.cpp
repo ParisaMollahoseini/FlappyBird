970660b11c2e664ac3936d68394fb5a5d8cf2dd1
Amir <coloner12@gmail.com>
2018-08-04 01:39:41 -0700

    change in moving bird

--- bird.cpp
+++ bird.cpp
@@ -8,12 +8,13 @@ bird::bird()
 
 void bird::keyPressEvent(QKeyEvent *event)
 {
-if(event->key()==Qt::Key_Up)
-{
-setY(pos().y()-40);
-}
-else if(event->key()==Qt::Key_Down)
-{
-setY(pos().y()+40);
-}
+    /*if(event->key()==Qt::Key_Up)
+    {
+        setY(pos().y()-40);
+    }
+
+    else if(event->key()==Qt::Key_Down)
+    {
+        setY(pos().y()+40);
+    }*/
 }
